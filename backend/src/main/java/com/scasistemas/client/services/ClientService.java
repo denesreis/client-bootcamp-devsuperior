98e7126316e7aed78637ebd35d5df2cf3607e4e6
“Denes <denesreis@scasistemas.com.br>
2021-02-24 18:24:00 -0300

    Com auditoria e get por id

--- backend/src/main/java/com/scasistemas/client/services/ClientService.java
+++ backend/src/main/java/com/scasistemas/client/services/ClientService.java
@@ -1,6 +1,6 @@
 package com.scasistemas.client.services;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import com.scasistemas.client.dto.ClientDTO;
 import com.scasistemas.client.entities.Client;
 import com.scasistemas.client.repositories.ClientRepository;
+import com.scasistemas.client.services.Exception.ResourceNotFoundException;
+
 
 @Service
 public class ClientService {
@@ -22,6 +24,18 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest);
 			
 		return list.map(x -> new ClientDTO( x ));
+		
+	}
+
+	public ClientDTO findById(Long id) {
+		
+		Optional<Client> obj = repository.findById(id);
+		
+		//Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não localizado"));
+		
+		Client entity = obj.get();
+		
+		return new ClientDTO(entity);
 		
 	}
 	
